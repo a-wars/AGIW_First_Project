d539f5f42d2f08be2a7100bc6852e353c264bd8c
Vincenzo <vmartello.95@gmail.com>
2019-05-09 18:27:46 +0200

    aggiunto algoritmo di clustering

--- feature_generator/demo.py
+++ feature_generator/demo.py
@@ -1,19 +1,53 @@
-from features import TagFrequency
+from feature_generator import features
+from feature_generator import utility
+from sklearn.cluster import MeanShift
 import numpy as np
+import pandas as pd
 
+#cluster di cataloghi e di prodotti
+def count_occurrences(vec,n):
+	occ=0
+	for el in vec:
+		if el==n:
+			occ=occ+1
+	return occ
 
-html_src1='<html><body><ul class="list1"><li>A</li><li>B</li></ul><ul class="list2"><li>Y</li><li>Z</li></ul></body></html>'
-html_src2='<html><body><p>Another page with a paragraph tag </p></body></html>'
-vectorizer=TagFrequency()
 
 matrix=[]
-feature_vec1=np.array(vectorizer(html_src1))
-feature_vec2=np.array(vectorizer(html_src2))
+vectorizer=features.TagFrequency()
 
-matrix.append(feature_vec1)
-matrix.append(feature_vec2)
-for vec in matrix:
-    vec=np.pad(vec,(0,3),'constant')
-    print(type(vec))
-    print(vec)
-    print('\n')
+df=pd.read_csv('/home/vincenzo/PycharmProjects/AGIW_First_Project/dataset/bookrepository.csv',nrows=1000)
+fmt_string = 'There are {} row with {} label'
+print(fmt_string.format(len(df[df['label'].isnull()]),'no'))
+print(fmt_string.format(len(df[df['label']=='product']), 'product'))
+
+X=df['src']
+last_vec=None
+
+for doc in X:
+	feature_vec=vectorizer(doc)
+	matrix.append(feature_vec)
+	last_vec=feature_vec
+
+
+print('lunghezza matrice:'+str(len(matrix))+'\n')
+utility.pad_matrix_elem(matrix, last_vec)
+
+numpymatrix=np.array(matrix)
+clustering = MeanShift(bandwidth=0.1).fit(numpymatrix)
+labels=clustering.labels_
+print(labels)
+
+df['predicted_labels'] = labels
+
+nclusters=len(clustering.cluster_centers_)
+selectedelements=count_occurrences(labels,0)
+truepositive=0
+allpositives=len(df[df['label']=='product'])
+for index, row in df.iterrows():
+	if row['label']=='product' and row['predicted_labels']==0:
+		truepositive+=1
+
+fmt_string='{} is {}'
+print(fmt_string.format('Recall', truepositive/allpositives))
+print(fmt_string.format('Precision', truepositive/selectedelements))
