1dfb7dab19065d2a999ddffb92fa967e66872cec
Vincenzo <vmartello.95@gmail.com>
2019-05-13 18:45:24 +0200

    modifiche per stampare recall e precision anche sui cataloghi

--- astarwars_clustering/clustering/clusteringtest.py
+++ astarwars_clustering/clustering/clusteringtest.py
@@ -4,25 +4,34 @@ from astarwars_clustering.utils import utility
 from astarwars_clustering.clustering import structural_clustering
 
 FILEPATH = '/home/vincenzo/PycharmProjects/AGIW_First_Project/datasets/blackwells.csv'
-df = pd.read_csv(FILEPATH,nrows=2000)
+df = pd.read_csv(FILEPATH,nrows=4000)
 print("No. of rows and columns")
 print("-----------------------")
 print(df.shape)
 
-
-clusters=structural_clustering.structural_clustering(df['src'])
+featureMat=structural_clustering.createFeatureMatrix(df['src'])
+clusters=structural_clustering.structural_clustering(bandwidth=0.07,featurematrix=featureMat)
 labels=clusters.labels_
 print (np.unique(labels))
 
 nclusters=len(clusters.cluster_centers_)
 df['predicted_labels']=labels
-selectedelements=utility.count_occurrences(labels, 0)
-truepositive=0
-allpositives=len(df[df['label']=='product'])
+selectedelementsproducts=utility.count_occurrences(labels, 0)
+selectedelementcatalog=utility.count_occurrences(labels,1)
+truepositivecatalog=0
+truepositiveproduct=0
+allpositivesproduct=len(df[df['label'] == 'product'])
+allpositivecatalog=len(df[df['label'] == 'list'])
 for index, row in df.iterrows():
-	if row['label']=='product' and row['predicted_labels']==0:
-		truepositive+=1
+	if row['label'] == 'product' and row['predicted_labels'] == 0:
+		truepositiveproduct += 1
+	if row['label'] == 'list' and row['predicted_labels'] == 1:
+		truepositivecatalog += 1
+
 
 fmt_string='{} is {}'
-print(fmt_string.format('Recall', truepositive/allpositives))
-print(fmt_string.format('Precision', truepositive/selectedelements))
+print(fmt_string.format('Recall', truepositiveproduct / allpositivesproduct))
+print(fmt_string.format('Precision', truepositiveproduct / selectedelementsproducts))
+
+print(fmt_string.format('Recall', truepositivecatalog / allpositivecatalog))
+print(fmt_string.format('Precision', truepositivecatalog / selectedelementcatalog))
--- astarwars_clustering/clustering/structural_clustering.py
+++ astarwars_clustering/clustering/structural_clustering.py
@@ -1,25 +1,51 @@
 from astarwars_clustering.clustering import features
 from astarwars_clustering.utils import utility
-from sklearn.cluster import MeanShift,estimate_bandwidth
+from sklearn.cluster import MeanShift, estimate_bandwidth
 import numpy as np
 import pandas as pd
+import time as time
 
-#clustering algorithm
-#It takes as input a Pandas Series containing html source code and a hyperparameter for mean shift clustering algorithm called bandwith
-def structural_clustering(bandwidth,featurematrix):
-	
-	clustering = MeanShift(bandwidth=bandwidth).fit(featurematrix)
 
-	return clustering
+# clustering algorithm
+# It takes as input a Pandas Series containing html source code and a hyperparameter for mean shift clustering algorithm called bandwith
+
+
+def structural_clustering(bandwidth, featurematrix):
+    start = time.time()
+    clustering = MeanShift(bandwidth=bandwidth).fit(featurematrix)
+    end = time.time()
+    hours, rem = divmod(end - start, 3600)
+    minutes, seconds = divmod(rem, 60)
+    print("Elapsed time to calculate clustering:{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
+    return clustering
 
 
 def createFeatureMatrix(listOfHtmls):
-	matrix = []
-	vectorizer = features.TagFrequency()
-	last_vec = None
-	for _, doc in listOfHtmls.iteritems():
-		feature_vec = vectorizer(doc)
-		matrix.append(feature_vec)
-		last_vec = feature_vec
-	utility.pad_matrix_elem(matrix, last_vec)
-	return matrix
+    start = time.time()
+
+    matrix = []
+    vectorizer = features.TagFrequency()
+    last_vec = None
+    for _, doc in listOfHtmls.iteritems():
+        feature_vec = vectorizer(doc)
+        matrix.append(feature_vec)
+        last_vec = feature_vec
+    utility.pad_matrix_elem(matrix, last_vec)
+
+    end = time.time()
+    hours, rem = divmod(end - start, 3600)
+    minutes, seconds = divmod(rem, 60)
+    print("Elapsed time to calculate features:{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
+    return matrix
+
+
+def calculatePrecision(truepositive, selectedelements):
+    return truepositive / selectedelements
+
+
+def calculateRecall(truepositive,allpositive):
+	return truepositive/allpositive
+
+def calculateF1Score(recall,precision):
+	return 2*((recall*precision)/(recall+precision))
+
